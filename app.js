39cd0b7f95084b1199c72bfeca1f603bf12f41b3
Andres <floresc.andres@intracitygeeks.org>
2018-05-10 22:54:25 +0000

    used the form to add li and span elements to the book list

--- app.js
+++ app.js
@@ -123,6 +123,22 @@ const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
    e.preventDefault();
    const value = addForm.querySelector('input[type="text"]').value;
-   console.log(value);
-});
 
+
+// ----- Lesson 12 -----
+
+   //create elements
+   
+   const li = document.createElement('li');
+   const bookName = document.createElement('span');
+   const deleteBtn = document.createElement('span');
+   
+   //add content
+   deleteBtn.textContent = 'delete';
+   bookName.textContent = value;
+   
+   //append to document
+   li.appendChild(bookName);
+   li.appendChild(deleteBtn);
+   list.appendChild(li);
+});
